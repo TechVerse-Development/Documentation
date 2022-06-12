e7a306f735d4ad0015dc7719542dbcea48690234
Techout <root@techout.tech>
2022-06-12 21:22:31 +0200

    Update index.js

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -35,11 +35,11 @@ const FeatureList = [
 	},
 ];
 
-function Feature({ Svg, title, description }) {
+function Feature({ title, description }) {
 	return (
 		<div className={clsx("col col--4")}>
 			<div className="text--center">
-				<Svg className={styles.featureSvg} role="img" />
+				//<Svg className={styles.featureSvg} role="img" />
 			</div>
 			<div className="text--center padding-horiz--md">
 				<h3>{title}</h3>
