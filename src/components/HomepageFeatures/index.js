ba9c59be82eefbbb12d30c0a95da7bff355703dd
Eight∞ <wangzndg@gmail.com>
2022-06-12 21:28:38 +0200

    rm: svg in landing page

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -5,17 +5,16 @@ import styles from "./styles.module.css";
 const FeatureList = [
 	{
 		title: "Globally Available",
-		// Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
 		description: (
 			<>
-				Our services are geo-routed to ensure high speeds and low latency. Wherever
-				you are in the world, you'll still get low latency while using our services.
+				Our services are geo-routed to ensure high speeds and low latency.
+				Wherever you are in the world, you'll still get low latency while using
+				our services.
 			</>
 		),
 	},
 	{
 		title: "99.9% Availability",
-		// Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
 		description: (
 			<>
 				We guarantee 99.9% availability. With our systems, we can guarantee that
@@ -25,11 +24,11 @@ const FeatureList = [
 	},
 	{
 		title: "Secure",
-		// Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
 		description: (
 			<>
 				We're always taking steps to review and improve security as we know how
-				important your data is. Apart from that, any data submitted to us is encrypted.
+				important your data is. Apart from that, any data submitted to us is
+				encrypted.
 			</>
 		),
 	},
@@ -38,9 +37,7 @@ const FeatureList = [
 function Feature({ title, description }) {
 	return (
 		<div className={clsx("col col--4")}>
-			<div className="text--center">
-				//<Svg className={styles.featureSvg} role="img" />
-			</div>
+			<div className="text--center"></div>
 			<div className="text--center padding-horiz--md">
 				<h3>{title}</h3>
 				<p>{description}</p>
