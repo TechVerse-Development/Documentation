15200e305cb1fce73592ed2afbbc40163d705c29
Techout <root@techout.tech>
2022-06-12 21:18:37 +0200

    Update index.js

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -5,32 +5,31 @@ import styles from "./styles.module.css";
 const FeatureList = [
 	{
 		title: "Globally Available",
-		Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
+		// Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
 		description: (
 			<>
-				Our API is geo-routed to ensure high speeds and low latency. Wherever
-				you are in the world, you'll still get low latency while using our API.
+				Our services are geo-routed to ensure high speeds and low latency. Wherever
+				you are in the world, you'll still get low latency while using our services.
 			</>
 		),
 	},
 	{
 		title: "99.9% Availability",
-		Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
+		// Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
 		description: (
 			<>
-				We guarantee 99.9% availability. With our systems, we can recommend that
+				We guarantee 99.9% availability. With our systems, we can guarantee that
 				we won't be going down without a valid reason.
 			</>
 		),
 	},
 	{
 		title: "Secure",
-		Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
+		// Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
 		description: (
 			<>
 				We're always taking steps to review and improve security as we know how
-				important your data is. Apart from that, we have all your data
-				encrypted.
+				important your data is. Apart from that, any data submitted to us is encrypted.
 			</>
 		),
 	},
