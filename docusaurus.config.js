311ecf89563d1596be508d3f743fd0fc89aa0016
Eight∞ <wangzndg@gmail.com>
2022-06-17 15:11:36 +0200

    feat: add favicon and logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,119 +6,119 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-	title: "TechVerse",
-	tagline:
-		"TechVerse Development is an organisation dedicated to developing new, useful software.",
-	url: "https://www.techversedev.xyz",
-	baseUrl: "/",
-	onBrokenLinks: "throw",
-	onBrokenMarkdownLinks: "warn",
-	favicon: "img/favicon.ico",
+  title: "TechVerse",
+  tagline:
+    "TechVerse Development is an organisation dedicated to developing new, useful software.",
+  url: "https://www.techversedev.xyz",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
 
-	// GitHub pages deployment config.
-	// If you aren't using GitHub pages, you don't need these.
-	organizationName: "TechVerse-Development", // Usually your GitHub org/user name.
-	projectName: "", // Usually your repo name.
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "TechVerse-Development", // Usually your GitHub org/user name.
+  projectName: "", // Usually your repo name.
 
-	// Even if you don't use internalization, you can use this field to set useful
-	// metadata like html lang. For example, if your site is Chinese, you may want
-	// to replace "en" with "zh-Hans".
-	i18n: {
-		defaultLocale: "en",
-		locales: ["en"],
-	},
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
 
-	presets: [
-		[
-			"classic",
-			/** @type {import('@docusaurus/preset-classic').Options} */
-			({
-				docs: {
-					sidebarPath: require.resolve("./sidebars.js"),
-					// Please change this to your repo.
-					// Remove this to remove the "edit this page" links.
-					editUrl:
-						"https://github.com/TechVerse-Development/Documentation/tree/main",
-				},
-				blog: {
-					showReadingTime: true,
-					// Please change this to your repo.
-					// Remove this to remove the "edit this page" links.
-					editUrl:
-						"https://github.com/TechVerse-Development/Documentation/tree/main",
-				},
-				theme: {
-					customCss: require.resolve("./src/css/custom.css"),
-				},
-			}),
-		],
-	],
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          // editUrl:
+          // 	"https://github.com/TechVerse-Development/Documentation/tree/main",
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            "https://github.com/TechVerse-Development/Documentation/tree/main",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
 
-	themeConfig:
-		/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-		({
-			navbar: {
-				title: "TechVerse",
-				logo: {
-					alt: "TechVerse Logo",
-					src: "img/logo.svg",
-				},
-				items: [
-					{
-						type: "doc",
-						docId: "intro",
-						position: "left",
-						label: "API",
-					},
-					{
-						href: "https://github.com/TechVerse-Development",
-						label: "GitHub",
-						position: "right",
-					},
-				],
-			},
-			footer: {
-				style: "dark",
-				links: [
-					{
-						title: "Docs",
-						items: [
-							{
-								label: "API",
-								to: "/docs/intro",
-							},
-						],
-					},
-					{
-						title: "Community",
-						items: [
-							{
-								label: "Discord",
-								href: "https://techversedev.xyz/r/discord",
-							},
-							{
-								label: "Twitter",
-								href: "https://twitter.com/TechVerse4/",
-							},
-						],
-					},
-					{
-							title: "Other",
-						items: [
-							{
-								label: "GitHub",
-								href: "https://github.com/TechVerse-Development",
-							},
-						],
-					},
-				],
-				copyright: `Copyright © ${new Date().getFullYear()} TechVerse.`,
-			},
-			prism: {
-				theme: lightCodeTheme,
-				darkTheme: darkCodeTheme,
-			},
-		}),
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: "TechVerse",
+        logo: {
+          alt: "TechVerse Logo",
+          src: "img/logo.png",
+        },
+        items: [
+          {
+            type: "doc",
+            docId: "intro",
+            position: "left",
+            label: "API",
+          },
+          {
+            href: "https://github.com/TechVerse-Development",
+            label: "GitHub",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Docs",
+            items: [
+              {
+                label: "API",
+                to: "/docs/intro",
+              },
+            ],
+          },
+          {
+            title: "Community",
+            items: [
+              {
+                label: "Discord",
+                href: "https://techversedev.xyz/r/discord",
+              },
+              {
+                label: "Twitter",
+                href: "https://twitter.com/TechVerse4/",
+              },
+            ],
+          },
+          {
+            title: "Other",
+            items: [
+              {
+                label: "GitHub",
+                href: "https://github.com/TechVerse-Development",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} TechVerse.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
 };
 
 module.exports = config;
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,32 +8,32 @@ import HomepageFeatures from "@site/src/components/HomepageFeatures";
 import styles from "./index.module.css";
 
 function HomepageHeader() {
-	const { siteConfig } = useDocusaurusContext();
-	return (
-		<header className={clsx("hero hero--primary", styles.heroBanner)}>
-			<div className="container">
-				<h1 className="hero__title">{siteConfig.title}</h1>
-				<p className="hero__subtitle">{siteConfig.tagline}</p>
-				<div className={styles.buttons}>
-					<Link
-						className="button button--secondary button--lg"
-						to="/docs/intro"
-					>
-						Jump into the Documentation!
-					</Link>
-				</div>
-			</div>
-		</header>
-	);
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/intro"
+          >
+            Jump into the Documentation!
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default function Home() {
-	return (
-		<Layout description="TechVerse Development is an organisation dedicated to developing new, useful software.">
-			<HomepageHeader />
-			<main>
-				<HomepageFeatures />
-			</main>
-		</Layout>
-	);
+  return (
+    <Layout description="TechVerse Development is an organisation dedicated to developing new, useful software.">
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
 }
